6aefeba1e2a39d589aded450460a6044d6e57b4d
journeythrunrun <164328543+journeythrunrun@users.noreply.github.com>
2024-03-31 18:23:46 +0900

    Add study_aweek_problems

--- study/AA_SILVER34_04_studentnumber.py
+++ study/AA_SILVER34_04_studentnumber.py
@@ -1,4 +1,5 @@
 # https://www.acmicpc.net/problem/1235
+# 코드 정리 덜함
 # 0)
 # 3
 # 1212345
@@ -24,29 +25,27 @@ k=0
 target=[]
 person_set = []
 
-for i in range(1,len(students[0])+1):# 자릿수 증가 # 아 n이 사람수였네1!!!!!
-
+for i in range(1,len(students[0])+1):# 자릿수 증가
     k+=1
-
-    # n_decimal = [0] * 10
     if len(person_set)==0: #
         person_set=[ range(n) ]#~ # person_set # 그대로 [ range(n) ] 임. list()랑은 다름.
     # else :
     #     persons= target[exam]
     exist=False
 
-    # 갑자기 indent추가해줄 때 , 초기화 위치 단락이동해줄 때, 단순복사 말고 각 변수 의미 및 앞뒤 layer 체크
-    exam = set()  # 뒤에 for에 쓰이기 때문에[이건 한 bit당] 거기 넘는 level 초기화 해야함 # 3,4개이상인놈들 2,3번 append시 중복피하기 위해
-    # old_set=person_set[:] # 주소 안 가지도록
+    # * 갑자기 tap 추가해줄 때 , 초기화 위치 단락이동해줄 때, 단순복사 말고 각 변수 의미 및 앞뒤 layer 체크
+    # old_set=person_set[:] # 2차원이라.., # 주소 안 가지도록
+    temp=[]
     while(person_set): # digit 당
-        persons=person_set.pop() # [사람들1,4,5 ]
-        print(persons)
+        persons=person_set.pop() # [사람들1,4,5 ] [2,3]
+        # print(persons)
         target = [[] for _ in range(10)]  # '그 숫자'로 같은 놈들끼리' 행으로 저장 -> 검사
         seta = set()
+        exam = set()  # 뒤에 for에 쓰이기 때문에[이건 한 bit당] 거기 넘는 level 초기화 해야함 # 3,4개이상인놈들 2,3번 append시 중복피하기 위해
 
         for person in persons: # n 1000이니까 그냥 다 돌자 # index+=1 # for로 다 돌진 않음
             str_numb = students[person][-i] # 3
-            print('why', person,str_numb,seta) # 다른 person_set의 다른 행으로 부터 같은 digit으로 겹치면 꼬임
+            # print('why', person,str_numb,seta) # 다른 person_set의 다른 행으로 부터 같은 digit으로 겹치면 꼬임
             target[int(str_numb)].append(person)  # 첫번째 사람인덱스도 저장하기 위하여 앞에서 append
             # 아래 그냥 str로 그대로 사용
             if str_numb  not in seta : # 처음 보는 숫자
@@ -58,19 +57,22 @@ for i in range(1,len(students[0])+1):# 자릿수 증가 # 아 n이 사람수였�
                 # 타겟 행돌며 길이가 2이상일 떄 검사 or<--=> 검사대상 exam에 사람만 추가
                 exist=True
             # print('exam',exam)
-        # 중간에 생각했던 거긴하네. 그때 old_set 하려다가 왜 안해결하고 넘어갔었지.. #! 하고 해결하고 넘어가라 다른 원인 아니고 이것도 해결안됐었다
-        # <-> while조건문
-        # person_set pop으로 자동 초기화
-    for ex in exam : # 해당 비트 다 끝나고 나서 for ###~
-        person_set.append( target[ex] ) # 해당 행=사람들만 붙여줌 #~ target[ex] 가 같은 digit이 다른 person집합에서 출현시 덮여써져버림
 
-    print ('뒤에서 i번째 같은놈들', i, person_set,exist) #  aaaa 3 []  / X_ aaaa 5 [[0, 1]] True 왜 5에서 끝나나했더니 n잘못봄
+        for ex in exam : # 해당 비트 다 끝나고 나서 for ###~
+            temp.append( target[ex] ) # 해당 행=사람들만 붙여줌 #~ target[ex] 가 같은 digit이 다른 person집합에서 출현시 덮여써져버림
+    for t in temp:
+        person_set.append(t)
+
+    # print ('뒤에서 i번째 같은놈들', i, person_set,exist) #  aaaa 3 []  / X_ aaaa 5 [[0, 1]] True 왜 5에서 끝나나했더니 n잘못봄
     if exist==False: #  한번이라도 같은 경우 계속 하기 <-> 다다르면 break
         break
-
 print(k, end='')
 
+# - 2h 3m _ 디버깅
+#  + for에서 n을 자릿수 개수로 써버림..
+#  + 답맞았는데 쌩쑈시간++[6번쨰까지 같은놈들이면 7번쨰가맞지] # - 내가 임의로 만든 예제_답 문제기반 E2E엄밀 생각 # 코드 좀더 다듬게되긴했지만 난 답만 맞추면되는걸
+#  + person_set 추가 및 while조건 엉킴
+#   중간에 생각하고 old_set으로 해결 하려다가 다른 거 해결로 넘어가버린듯
+# #! 하고 해결 스택에 추가해라.. 다른 원인 아니고 이것'도' 해결안됐었다
 
-# 디버깅 : 왜 있던 같은 요소에서 다른 거가되지 똑같이 안되지
 
-#
