9eab6b514e0d266e6558be5edc36d6a16e6f613e
journeythrunrun <164328543+journeythrunrun@users.noreply.github.com>
2024-04-05 21:28:27 +0900

    Update 요세푸스 문제.py
    Edit annotations

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
@@ -4,18 +4,18 @@
 # 7 3
 # 예제 출력 <3, 6, 2, 7, 5, 1, 4>
 
-# 1) N명 '원' _ K번쨰 사람 제거 반복 _ 사람 다 제거 될 때까지
+# 1) N명 '원' _ K번째 사람 제거 반복 _ 사람 다 제거 될 때까지
 # -> 제거된 순서
 
 # 2) 시복 널널할듯_k, n 둘 다 범위 5000까지임
 # m1) pop, remove _ 문제 푸는 속도 + 부분점수
 # m2) index
 # m) 대상 남아있는거 지나면서 카운팅
-# m) 각 위치인덱스에 인덱스로 더할 값 저장 ㅁ 0, 1
+# m)[이 방법씀] 각 위치인덱스에 인덱스로 더할 값 저장  0, 1
 
 n,k = map(int, input().split())
 target=list(range(1,n+1))
-icount=[1]*n # 인덱스 0으로 땡겨서씀. 사람은 1부터임.
+icount=[1]*n # 인덱스 0으로 땡겨서씀. ( 사람 n명 )
 i=0
 
 index=0
@@ -24,12 +24,12 @@ print('<',end='')
 for time in range(n):# <->m2)pop_while (target):
     # 새 인덱스 찾기
     fork=0
-    j=index
-    while (fork<k):  # k가 되면 탈출
-        # if index not in seta:
-        fork+=icount[j] # 0 1
-        index = j
-        j= 0 if j+1 == n else j+1 # 다음 인덱스
+    j=index # 진짜 index는 남겼으며 한 명씩 건너갈 j라는 임시 인덱스 사용
+    while (fork<k):  # k가 되면 탈출 [k개 건너감]
+        # if index not in seta: # 다른 방법 쓰려던 거 남겨둠
+        fork+=icount[j] # 0_이미 제거 된 애라 카운팅 포함 안됨 1 
+        index = j # 그냥 index로만 해도 되는데 문제 성공했던 코드를 보존하고자 추가 압축 안했음(그때도 문제 풀이 시간 때문에 추가 압축에 시간 들이진 않음)
+        j= 0 if j+1 == n else j+1 - 다음 인덱스 / 맨 끝에서 0으로 돌아오는 거 주의
         # index+=1
     # index=j
 
@@ -44,7 +44,7 @@ for time in range(n):# <->m2)pop_while (target):
     #         index= (k*i)%len(target) # <-> index()
     #     j+=1
     icount[index]=0
-    if time<n-1:
+    if time<n-1: # 마지막 출력에서의 '>'를 위해 출력 분리 
         print(target[index],', ',sep='',end='')# m2)target.pop(index)) # 포맷팅 복습?
     # i+=1
     #
