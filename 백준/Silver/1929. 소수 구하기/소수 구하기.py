8bc7b7cff25b3ac2723958585a610961c01d02be
journeythrunrun <164328543+journeythrunrun@users.noreply.github.com>
2024-04-06 00:05:40 +0900

    Update 소수 구하기.py

--- 백준/Silver/1929. 소수 구하기/소수 구하기.py
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -1,13 +1,14 @@
 # https://www.acmicpc.net/problem/1929
 
 m,n = map(int, input().split())
-# m1) 각 수의 배수 뺴기
+# m1) 각 수의 배수 빼기
 # m2) append -> 시복을 위한 set.add
 
 # 2) 일단 시복 최적화는 무시하고 풀어볼까
 seta=set()
 seta.add(1)# 1 소수 아님!!!!!!!!!
-for numb in range (2, (n)//2+1 ): # seta는 제외대상이기 때문에 검사대상의 끝이 몇 개 더 추가 돼도 괜찮음.
+# - 시간복잡도 : 딕셔너리, set
+for numb in range (2, (n)//2+1 ): # seta는 제외대상이기 때문에 numb 몇 개 더로 인해 검사대상의 끝이 더 추가 돼도 결과 영향없음.
     i=2
     while(numb*i <(n)+1 ):
         seta.add(numb*i)
@@ -26,3 +27,35 @@ for numb in range (m, (n+1) ):
 #   + 스터디 문제 설명의 힌트인 "에라토스테네스의 체" 개념에 대한 공부를 하지 않고 풀어봄 # 단, 체라는 단어를 듣고 과거의 알고리즘 기억이 조금 떠올랐을 수 있음
 
 # - 집에서 코딩하면 문제 풀다가 다른 생각 드넹 집중
+
+
+# - 타인 [ssaemo] : 어차피 볼 거면 codility 코드가 더 낫다
+# import sys
+# readl = sys.stdin.readline
+# def f():
+#     candidates = [1] * (n+1)
+#     candidates[1] = 0
+#     i = 2
+#     while i**2 <= n:
+#         if candidates[i]:
+#             ii = i*i
+#             while ii <= n:
+#                 candidates[ii] = 0
+#                 ii += i
+#         i += 1
+#     prime_nums = []
+#     if m <= 2:
+#         prime_nums.append('2')
+#     if m % 2 == 0:
+#         mm = m+1
+#     else:
+#         mm = m
+#     for i in range(mm, n+1, 2):
+#         if candidates[i]:
+#             prime_nums.append(str(i))
+#     s = '\n'.join(prime_nums)
+#     sys.stdout.write(s)
+
+
+# m, n = [int(i) for i in readl().split()]
+# f()
