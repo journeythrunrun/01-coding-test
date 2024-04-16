c637f3fea32d1c267f2c804906ad88c27a68e9f5
Sohyeon Yang <164328543+journeythrunrun@users.noreply.github.com>
2024-04-17 01:14:21 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 0.20 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.py
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.py
@@ -4,13 +4,10 @@
 #    2. 길이를 2진법으로 표현한 문자열 # 4 ->100 
 #    > s가 '1'이 될 때까지 [변환 횟수, 제거된 0수]
 
-# 2) 
-
-    
+# 2)     
 def solution(s):
     answer = [0,0]
     # a=''.join(list(reversed('abc'))) # 개수가 필요한거라 reverse 할 필요없음
-    # print(a)
     count=0
     while(1):
         
@@ -34,3 +31,16 @@ def solution(s):
         s=temp
     
     return answer
+
+# - 20m / +1
+# - 바이너리화 함수 bin()[2:]_문자열임 & 앞에 0b있기 때문에 슬라이싱
+
+# - 다른 사람 코드 [ 정윤성 , 이유진 , 김상엽 , aika1118 외 75 명 ]
+# def solution(s):
+#     a, b = 0, 0
+#     while s != '1':
+#         a += 1
+#         num = s.count('1')
+#         b += len(s) - num
+#         s = bin(num)[2:]
+#     return [a, b]
