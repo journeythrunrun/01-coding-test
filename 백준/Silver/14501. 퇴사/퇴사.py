9085cf2e56f2e73535b12c47b0e435e39856b0bf
Sohyeon Yang <164328543+journeythrunrun@users.noreply.github.com>
2024-05-06 12:40:52 +0900

    [Silver III] Title: 퇴사, Time: 112 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.py
+++ 백준/Silver/14501. 퇴사/퇴사.py
@@ -10,6 +10,8 @@
 # dp bfs로도
 
 # - dp 빠르게 학습할 생각으로 19m풀다가 정답봄[이것이 코딩테스트다 깃허브]
+#   + dp : 점화식 & DP테이블(해당까지의 maxmin,...) 생각
+# - 문제 -> 해당 t까지의 max를 연이어나갈 수 있음
 n = int(input()) # 전체 상담 개수
 t = [] # 각 상담을 완료하는데 걸리는 기간
 p = [] # 각 상담을 완료했을 때 받을 수 있는 금액
@@ -22,13 +24,13 @@ for _ in range(n):
     p.append(y)
 
 # 리스트를 뒤에서부터 거꾸로 확인
-for i in range(n - 1, -1, -1):
-    time = t[i] + i ## time : 상담이 끝나는
+for i in range(n - 1, -1, -1): # 앞 일부터 시작할 수록 뒷일 돈까지 받을 수 있는거라, 앞쪽 총돈이 누적이라 뒤쪽부터 계산
+    time = t[i] + i ## time : 상담이 끝난 다음날. 시간은 0~로 땡겨져있음 (다음 상담 가능일)
     # 상담이 기간 안에 끝나는 경우
-    if time <= n:
+    if time <= n: ## time이 다음날이라 n까지도 되는거임.
         # 점화식에 맞게, 현재까지의 최고 이익 계산
-        dp[i] = max(p[i] + dp[time], max_value)
-        max_value = dp[i]
+        dp[i] = max(p[i] + dp[time], max_value) # max( p[i]_이번타임에서 얻을 수 있는 가격반영 + dp[time]_이번타임할 시 바로 다음 타임에서 얻을 수 있던 최대값 ,maxv_이번 타임 안먹은 것과의 최대비교)
+        max_value = dp[i] # 이번 타임max도 고려된 총maxv 갱신
     # 상담이 기간을 벗어나는 경우
     else:
         dp[i] = max_value
@@ -53,11 +55,3 @@ print(max_value)
 #         for _ in af
 
 #     for 
-
-
-
-
-
-
-
-
