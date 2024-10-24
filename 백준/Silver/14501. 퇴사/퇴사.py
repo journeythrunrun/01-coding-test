a2ad8ba2e4ab0a377a803b68091e32077ec60fe0
Sohyeon Yang <164328543+journeythrunrun@users.noreply.github.com>
2024-10-24 15:14:56 +0900

    [Silver III] Title: 퇴사, Time: 92 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.py
+++ 백준/Silver/14501. 퇴사/퇴사.py
@@ -1,57 +1,44 @@
-# 0) 3 10(최대 1000), 
-# 1) N+1퇴사
-# ti-1개를 못하게됨
-# [] 구간
-# -> 최대수익
-# 2) 시간복잡도 널널
-# 완전탐색 -> [ti일 후:]부터 상담_ 가능한 세트->
-# ! 각 상담 일 선택시 그 후 가능한 바로 다음 타겟 따로 저장 & 그거 이후 for 이용
+# N+1일째 퇴사
+# 최대한 많은 상담
+# 하루에 하나씩 다른 상담
 
-# dp bfs로도
+# 2) O(2^n)
+# - 최대 수익
+# + dp[i]:i가 마지막으로 한 것일 경우의 총 수익 
+# + 첫Pi를 dp값초기화로?
+# + j<i : dp[i]= max(dp[i]_1일과4일일지 2일과 4일일지 for돌며 누적max검사, dp[j]+array[i][1]) if i>=j_0+array[j][0]_3조건만족# 이전꺼 할 수 있는지
+# + max(dp)
 
-# - dp 빠르게 학습할 생각으로 19m풀다가 정답봄[이것이 코딩테스트다 깃허브]
-#   + dp : 점화식 & DP테이블(해당까지의 maxmin,...) 생각
-# - 문제 -> 해당 t까지의 max를 연이어나갈 수 있음
-n = int(input()) # 전체 상담 개수
-t = [] # 각 상담을 완료하는데 걸리는 기간
-p = [] # 각 상담을 완료했을 때 받을 수 있는 금액
-dp = [0] * (n + 1) # 다이나믹 프로그래밍을 위한 1차원 DP 테이블 초기화
-max_value = 0
+# - k일동안 못함
+# - 마지막 상담의 N일째 이내에 끝나야함
+#   + 초기값 저장 시 : dp
+import sys
+inpu=sys.stdin.readline
+n=int(inpu()) 
+array= [list(map(int, inpu().split()) ) for _ in range( n) ]
 
-for _ in range(n):
-    x, y = map(int, input().split())
-    t.append(x)
-    p.append(y)
+# + 첫Pi를 dp값초기화로?
+#~ 미리 0으로 하면 중간인 케이스에서도 안되긴 하는데, 애초에 마지막도 안되는 경우면 중간도 안되는 거라 그게 맞음
+# 그 값을 array로는 더해도 되는 건지
+# <->걍!! 나중에 뺴기 
+dp=[ array[i][1] if n>=i+array[i][0] else 0 for i in range( len(array) )]
+# print(dp)
+for i in range(1,len(array)): # 첫놈은 앞놈이 없어서, 굳이 dp에 이전꺼 더하는거 할필요 없음 
+    if dp[i]==0: # 마지막이 이거인걸로 못끝남
+        continue
+    for j in range( i): # i-1 # 0은 굳이 안 써도
+        if i>=j+array[j][0] :#_3조건만족# 이전꺼 할 수 있는지
+            dp[i]= max(dp[i], dp[j]+array[i][1]) 
+    # print(dp)
+print(max(dp), end='')
+# + j<i : dp[i]= max(dp[i]_1일과4일일지 2일과 4일일지 for돌며 누적max검사, dp[j]+array[i][1]) 
+# + max(dp)
 
-# 리스트를 뒤에서부터 거꾸로 확인
-for i in range(n - 1, -1, -1): # 앞 일부터 시작할 수록 뒷일 돈까지 받을 수 있는거라, 앞쪽 총돈이 누적이라 뒤쪽부터 계산
-    time = t[i] + i ## time : 상담이 끝난 다음날. 시간은 0~로 땡겨져있음 (다음 상담 가능일)
-    # 상담이 기간 안에 끝나는 경우
-    if time <= n: ## time이 다음날이라 n까지도 되는거임.
-        # 점화식에 맞게, 현재까지의 최고 이익 계산
-        dp[i] = max(p[i] + dp[time], max_value) # max( p[i]_이번타임에서 얻을 수 있는 가격반영 + dp[time]_이번타임할 시 바로 다음 타임에서 얻을 수 있던 최대값 ,maxv_이번 타임 안먹은 것과의 최대비교)
-        max_value = dp[i] # 이번 타임max도 고려된 총maxv 갱신
-    # 상담이 기간을 벗어나는 경우
-    else:
-        dp[i] = max_value
-print(max_value)
+#~ 키보드 가져와서 무릎위에 두고할까
 
-# 이전 다른 방법
-# import sys
-# input=sys.stdin.readline
+# 4) N=1,..15
+# Ti,Pi 1,..
 
-# n=int(input())
-# data=[list(map(int,input().split())) for _ in range(n)]
-# after_target=[ i+data[i][0] for i in range(n)] # <->이후들도 다 저장할까
+# - 39m
 
-# print(after_target)
-# answer=0
-# for i in range(n): # 첫 시작 대상
-#     first=after_target[i]  # after_target에서 가져온 한 리스트의 순서대로 빼면서 쓰기 bfs유사
-#     asum=first[1]
-    
-#     # 재귀 대신 for : 저장
-#     while(1) :# 다음타겟인덱스가 n 이상이면 탈출 
-#         for _ in af
-
-#     for 
+#~ 0인결과 출력 가능. max가 0이라
