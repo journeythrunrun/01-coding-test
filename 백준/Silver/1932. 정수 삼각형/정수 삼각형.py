d6e269cb0f7ce0be76d74eb1b781b7e0fc334137
Sohyeon Yang <164328543+journeythrunrun@users.noreply.github.com>
2024-10-24 12:25:07 +0900

    [Silver I] Title: 정수 삼각형, Time: 124 ms, Memory: 111372 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.py
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.py
@@ -1,13 +1,9 @@
 # 1)
 # 합이 최대가 되는 경로
 
-
-
 # 2) 층 입력 받으며 . 층층이 저장해나가. 각경로. 
 # a 
 # b c : 
-
-
 
 # 비슷한거 풀때 난 아래층에서부터(큰거 선택하여 경우의 수 줄임).
 # - 아래층에서부터 c1 c2 중 큰 거 위로 c2 c3중에 큰 거 위로
@@ -15,15 +11,14 @@
 # for i in range( len(layers)-1 ) : for j in range( len(layers[-i-1])-1 ):
 # ## 7이면 그냥 더해주기 
 # layers[-i-2][j]+=max(layers[-i-1][j], layers[-i-1][j+1]) # - 뒤층 -(i+1) #layers[i][j]+=max(after_layer[j], after_layer[j+1])
-#~ 둘다 길이. 2층부터
+# 둘다 길이. 2층부터
 # (3) 누적되기 위해서 layer값으로하기
 n= int(input() )
 layers=[ list( map(int, input().split()) ) for _ in range(n) ]
-for i in range( len(layers)-1 ) : 
+for i in range( len(layers)-1 ) : # - 두 for 모두 : '길이' '-1' 돌기
     for j in range( len(layers[-i-1])-1 ):
         ## 7이면 그냥 더해주기->더해짐코드
-        layers[-i-2][j]+=max(layers[-i-1][j], layers[-i-1][j+1]) # - 뒤층 -(i+1) #layers[i][j]+=max(after_layer[j], after_layer[j+1])
-
+        layers[-i-2][j]+=max(layers[-i-1][j], layers[-i-1][j+1]) # - 아래층[뒤층]부터 : -(i+1) #layers[i][j]+=max(after_layer[j], after_layer[j+1])
 print(layers[0][0], end='')
 # 4) 층길이 1,... : 1 , 7 
 # 2
@@ -37,6 +32,7 @@ print(layers[0][0], end='')
 
 # - import sys
 # inpu=sys.stdin.readline
+
 # - 다른 사람 풀이
 '''
 import sys
@@ -54,6 +50,8 @@ for i in range(int(g())): # - n이라는 변수에 입력받은 값을 저장 �
 print(max(d))
 '''
 
-
-
-
+# - 커밋_백준 
+#   > 아마 주석 제외 실제 돌아가는 코드가 같으면 재커밋 안되나봄. 같은 페이지면 다시 열어서 제출해도, 로딩 너무 빨리 돌아가고 체크표시 뜨면서 제출 안된 상황
+#   + 앞으론 pypy -> python으로 완성본 제출
+#   > 예전 것도 수정본 업로드 안 된 것들 있겠네. 대체로 문제 들어가서 최근 꺼 보면 되긴함.
+  
